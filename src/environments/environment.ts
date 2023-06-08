edd0531acd4a67bb19399b275a81c650852e69fe
scottereynolds <32819847+scottereynolds@users.noreply.github.com>
2023-06-07 22:30:58 -0400

    Add files via upload
    Initial Commit - Working Login with Firebase and Google

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCGMjO067o80KbiNpP1fjhMxYxLzuoPBGA",
+    authDomain: "practicetwo-81806.firebaseapp.com",
+    projectId: "practicetwo-81806",
+    storageBucket: "practicetwo-81806.appspot.com",
+    messagingSenderId: "313215576018",
+    appId: "1:313215576018:web:5129ae39119c37bf9e4a0b",
+    measurementId: "G-0GVHEYYYCC"
+  }
+};
