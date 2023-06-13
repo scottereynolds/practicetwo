461d39234b31b8ccb9acc26266a45970db09c1e4
scottereynolds <32819847+scottereynolds@users.noreply.github.com>
2023-06-13 09:04:45 -0400

    Add files via upload
    Added proxy for api call. Made sure all pages are responsive.

--- src/app/shared/services/flightinfopayload.service.ts
+++ src/app/shared/services/flightinfopayload.service.ts
@@ -6,10 +6,10 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class FlightinfopayloadService {
-  private baseURL = `https://us-central1-crm-sdk.cloudfunctions.net/flightInfoChallenge`;
+  private baseURL = `/flightInfoChallenge/`;
   private token = `SGV5IFNjb3R0ISBJZiB5b3UgZGVjb2RlZCB0aGlzLCBwbGVhc2UgYnJpbmcgaXQgdXAgaW4gdGhlIGludGVydmlldyBmb3IgYm9udXMgcG9pbnRzISE`;
   postData(input: any): Observable<any> {    
-    const headers = { 'token': `${this.token}`, 'Access-Control-Allow-Origin': '*' };
+    const headers = { 'token': `${this.token}` };
     return this.http.post(`${this.baseURL}`, input, { headers });
   }
   constructor(private http: HttpClient) { }
